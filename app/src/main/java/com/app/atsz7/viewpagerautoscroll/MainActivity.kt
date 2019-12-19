946c3ec395a79a1103aef87ddf68b88006d37767
Atsz7 <angeltsz74@gmail.com>
2019-12-19 16:08:22 -0600

    Initial commit

---
+++ app/src/main/java/com/app/atsz7/viewpagerautoscroll/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.atsz7.viewpagerautoscroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
